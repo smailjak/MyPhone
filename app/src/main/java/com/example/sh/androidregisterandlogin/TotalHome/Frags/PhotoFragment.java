e626cdfc9b4fee325587f0b83753613ab29b6ff2
smailjak <46296836+smailjak@users.noreply.github.com>
2019-05-11 19:54:01 +0900

    Update PhotoFragment.java

--- app/src/main/java/com/example/sh/androidregisterandlogin/TotalHome/Frags/PhotoFragment.java
+++ app/src/main/java/com/example/sh/androidregisterandlogin/TotalHome/Frags/PhotoFragment.java
@@ -3,6 +3,8 @@ package com.example.sh.androidregisterandlogin.TotalHome.Frags;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,10 +13,7 @@ import android.view.ViewGroup;
 
 import com.example.sh.androidregisterandlogin.R;
 
-
-
 public class PhotoFragment extends Fragment {
-
 
     public static PhotoFragment newInstance() {
         return new PhotoFragment();
@@ -28,4 +27,11 @@ public class PhotoFragment extends Fragment {
     }
 
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
 }
